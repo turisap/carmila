c68ac1ec0b56289b100d78326871cfc229d1626a
Turisap <kirill-shakirov@mail.ru>
2017-12-10 22:55:48 +0700

    Datepicker semiready

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import ConfigureStore from './store/ConfigureStore';
-import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
-import 'react-dates/lib/css/_datepicker.css';
+import 'antd/dist/antd.css';
 
 const store = ConfigureStore();
 
--- src/components/pages/Order.js
+++ src/components/pages/Order.js
@@ -3,23 +3,46 @@
  */
 import React from 'react';
 import {connect} from 'react-redux';
+import { TimePicker } from 'antd';
+import moment from 'moment';
 import DishItem from '../DishItem';
 
 class Order extends React.Component {
+    state = {
+        inAppropriateTimeError : ''
+    };
+
+    onDateChange = (e) => {
+        console.log(e.isBefore(moment().add(30, ' minutes')))
+
+    };
     render () {
         return (
-            this.props.orders.map((item, i) => {
-                return (
-                    <div key={i}>
-                        <DishItem
-                            title={item.title}
-                            price={item.price}
-                            description={item.description}
-                            amount={item.amount}
-                        />
-                    </div>
-                )
-            })
+            <div>
+                {this.props.orders.map((item, i) => {
+                    return (
+                        <div key={i}>
+                            <DishItem
+                                title={item.title}
+                                price={item.price}
+                                description={item.description}
+                                amount={item.amount}
+                            />
+                        </div>
+
+                    )
+                })}
+                <form>
+                    <input name="name" type="text"/>
+                    <input name="phone" type="text"/>
+                    <TimePicker
+                        defaultValue={moment().add(1, 'hour')}
+                        format={'HH:mm'}
+                        onChange={this.onDateChange}
+                    />
+                </form>
+            </div>
+
         )
     }
 }
