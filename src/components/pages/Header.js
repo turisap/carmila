4fa956ff08483134bfe591738f2690a88c449b22
Turisap <kirill-shakirov@mail.ru>
2017-12-18 01:20:40 +0700

    Navbar collapsing

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -9,10 +9,6 @@ const Header = (props) => (
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-right">
             <NavLink className="navbar-brand" to="/">Caldira</NavLink>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
             <div className="collapse navbar-collapse" id="navbarColor03">
                 <ul className="navbar-nav">
                     <li className="nav-item">
