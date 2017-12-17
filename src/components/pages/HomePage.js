6d195d0edbdea80dc75b8ae73e66a62ddade5b64
Turisap <kirill-shakirov@mail.ru>
2017-12-17 12:31:23 +0700

    mobile opt homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -14,7 +14,7 @@ const HomePage = () => (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-8 offset-md-2">
-                    <p className="text-center">
+                    <p className="text-center intro__text">
                         We are a group of small take-away European restaurants in the
                         Battambang province and offer you a nice opportunity to try various items from Italian, Portuguese
                         German cuisine. We have five take-away points in the city and another one in the Veren village. We work 24/7
@@ -27,19 +27,19 @@ const HomePage = () => (
     <section id="homePagePhotos">
         <div className="container-fluid">
             <div className="row">
-                <div className="col-md-4">
-                    <img className="homePagePhotos_image homePagePhotos_main" src={"./img/pages/fire.jpg"}/>
+                <div className="col-4 homePagePhotos__column">
+                    <img className="homePagePhotos__image " src={"./img/pages/fire.jpg"}/>
                 </div>
-                <div className="col-md-4">
-                    <div className="homePagePhotos_cropper">
-                        <img className="homePagePhotos_image"  src={"./img/pages/dessert.jpg"}/>
-                        <img className="homePagePhotos_image"  src={"./img/pages/pancakes.jpg"}/>
+                <div className="col-4 homePagePhotos__column">
+                    <div className="homePagePhotos__cropper">
+                        <img className="homePagePhotos__image"  src={"./img/pages/dessert.jpg"}/>
+                        <img className="homePagePhotos__image"  src={"./img/pages/pancakes.jpg"}/>
                     </div>
                 </div>
-                <div className="col-md-4">
-                    <div className="homePagePhotos_cropper">
-                        <img className="homePagePhotos_image"  src={"./img/pages/pizza_home.jpg"}/>
-                        <img className="homePagePhotos_image"  src={"./img/pages/salami.jpg"}/>
+                <div className="col-4 homePagePhotos__column">
+                    <div className="homePagePhotos__cropper">
+                        <img className="homePagePhotos__image"  src={"./img/pages/pizza_home.jpg"}/>
+                        <img className="homePagePhotos__image"  src={"./img/pages/salami.jpg"}/>
                     </div>
                 </div>
             </div>
